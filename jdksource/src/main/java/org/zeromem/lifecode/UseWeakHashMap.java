ce0a385e7e8ab3265765ef66b7374a05db262844
zeromem <zeromem@qq.com>
2018-02-22 17:03:19 +0800

    home coding.

--- jdksource/src/main/java/org/zeromem/lifecode/UseWeakHashMap.java
+++ jdksource/src/main/java/org/zeromem/lifecode/UseWeakHashMap.java
@@ -1,9 +1,7 @@
 package org.zeromem.lifecode;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.WeakHashMap;
-import java.util.concurrent.locks.Condition;
 
 /**
  * @author zeromem
@@ -11,19 +9,24 @@ import java.util.concurrent.locks.Condition;
  */
 public class UseWeakHashMap {
     public static void main(String[] args) throws InterruptedException {
-        Map<Integer, Integer> weakMap = new WeakHashMap<>();
-        Map<Integer, Integer> hashMap = new HashMap<>();
+        Map<Integer, String> weakMap = new WeakHashMap<>();
 
-        Integer k1 = 10, k2 = 20;
-        weakMap.put(k1, k1);
-        weakMap.put(k2, k2);
+        Integer k1 = 10;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 15000000; i++) {
+            sb.append(i);
+        }
+        String l = sb.toString();
+
+
+        weakMap.put(k1, l);
 
         k1 = null;
-        k2 = null;
+        l = null;
 
         System.gc();
 
-        Thread.sleep(10000);
+        Thread.sleep(8000);
         System.out.println(weakMap);
 
 
---
+++ jdksource/src/main/java/org/zeromem/lifecode/concurrent/ComplexDemo.java
@@ -0,0 +1,45 @@
+package org.zeromem.lifecode.concurrent;
+
+import java.util.concurrent.*;
+
+/**
+ * @author zeromem
+ * @date 2018/2/22
+ */
+public class ComplexDemo {
+    public static void main(String[] args) {
+        // 0. create task thread queue
+        BlockingQueue<Runnable> queue = new LinkedBlockingDeque<>();
+
+        // 1. create thread pool (ExecutorService)
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                10, 50,
+                10, TimeUnit.SECONDS,
+                queue);
+
+        // 2. create CompletionService
+        CompletionService<String> service = new ExecutorCompletionService<>(executor);
+
+        // create task
+        Callable<String> task = () -> "hello world";
+
+        // 3. submit task(callable task) to service, not to executor.
+        service.submit(task); // RejectionExecutionException
+
+        // ...
+
+        // 4. take result from service
+        int TASK_SIZE = 100;
+        for (int i = 0; i < TASK_SIZE; i++) {
+            try {
+                String result = service.take().get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                // task future may failed.
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
